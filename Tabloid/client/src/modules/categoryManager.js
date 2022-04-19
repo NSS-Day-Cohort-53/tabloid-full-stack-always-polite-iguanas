433d880e2534f8b96ab750e41645164cbe115181
DougSilvera <shortxjdoug@gmail.com>
2022-04-19 14:07:32 -0400

    finished add category functionality

--- Tabloid/client/src/modules/categoryManager.js
+++ Tabloid/client/src/modules/categoryManager.js
@@ -18,3 +18,24 @@ export const getAllCategories = () => {
     });
   });
 };
+
+export const addCategory = (category) => {
+    return getToken().then((token) => {
+      return fetch(_apiUrl, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(category)
+      }).then(resp => {
+        if (resp.ok) {
+          return resp.json();
+        } else if (resp.status === 401) {
+          throw new Error("Unauthorized");
+        } else {
+          throw new Error("An unknown error occurred while trying to save a new quote.");
+        }
+      });
+    });
+  };
